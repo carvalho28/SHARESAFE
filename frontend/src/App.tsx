cd2d82dc41f2e5d32f8e9d76a0766ace8f47a0f7
carvtmux-sessionizeralho28 <carvalhoo287@gmail.com>
2023-03-20 23:00:57 +0000

    Solved bug with main.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ export function App() {
   );
 }
 
-export function renderApp() {
+export function RenderApp() {
   return (
     <BrowserRouter>
       <App />
--- frontend/src/test/App.test.tsx
+++ frontend/src/test/App.test.tsx
@@ -1,11 +1,11 @@
 import { describe, it } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
-import { renderApp, App } from "../App";
+import { RenderApp, App } from "../App";
 
 describe("App", () => {
   it("renders", () => {
-    render(renderApp());
+    render(RenderApp());
     expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent("Home");
   });
 
