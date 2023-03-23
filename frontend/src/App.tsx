d959618ae245ee5057ee89538766b773231ae348
GuilhermeBarata <guilhermebarata300702@gmail.com>
2023-03-23 19:17:34 +0000

    Updated landing page and added register page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import NotFound from "./NotFound";
+import Register from "./Register";
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
--- frontend/src/Register.tsx
+++ frontend/src/Register.tsx
@@ -1,7 +1,7 @@
-import { Link } from "react-router-dom";
 import logo from "./images/Logo.png";
+import { Link } from 'react-router-dom';
 
-function Home() {
+const Register = () => {
   return (
     <div className="h-screen flex items-center justify-center">
 
@@ -9,20 +9,22 @@ function Home() {
         <img alt="Logo" src={logo} />
       </div>
 
-      <div className="h-[80%] bg-gray-100 w-[5px] rounded-lg"/>
+      <div className="h-[80%] bg-gray-200 w-[5px] rounded-lg" />
       
       <div className="pl-[200px]">
         <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
           <div>
-            <p className="text-6xl text-blue-900">Login</p>
+            <p className="text-6xl text-blue-900">Register</p>
           </div>
           <div className="pt-3">
-            <p className="text-xs font-bold">WELCOME BACK TO SHARESAFE</p>
+            <p className="text-xs font-bold">
+              WELCOME TO SHARESAFE, LET&apos;S GET STARTED
+            </p>
           </div>
           <div className="pt-5">
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-xl"
-              id="LGNnome"
+              id="RGTnome"
               type="text"
               placeholder="Nome"
             />
@@ -30,31 +32,44 @@ function Home() {
           <div className="pt-5">
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-xl"
-              id="LGNpassword"
+              id="RGTemail"
+              type="text"
+              placeholder="Email"
+            />
+          </div>
+          <div className="pt-5">
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-xl"
+              id="RGTpassword"
               type="password"
               placeholder="Password"
             />
           </div>
-          <div className="pt-1">
-            <p className="text-xs">Forgot your password? Click <a href="" className="font-bold">here</a></p>
+          <div className="pt-5">
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-xl"
+              id="RGTconfirmpassword"
+              type="password"
+              placeholder="Confirm password"
+            />
           </div>
           <div className="md:w-2/3 pt-5">
             <button
               className="shadow appearance-none border rounded w-full py-2 bg-blue-900 hover:bg-blue-600 text-white font-bold text-xl"
-              id="loginBtn"
+              id="registerBtn"
               type="button"
             >
-              Login
+              Register
             </button>
           </div>
           <div className="pt-1">
-              <p className="text-xs">Don&apos;t have an account? Click <Link to="register" className="font-bold">here.</Link></p>
+              <p className="text-xs">Already have an account? Click <Link to="/" className="font-bold">here.</Link></p>
           </div>
         </form>
       </div>
 
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Register
