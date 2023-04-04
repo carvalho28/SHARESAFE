a653a68ea42c6867a56dc329b5f8060a918cfaa8
Simão <simaomarques20@gmail.com>
2023-04-04 15:53:39 +0100

    feat: fixed test errors

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../images/Logo.png";
-import SendFilePopup from "./sendFilePopup";
+import SendFilePopup from "./SendFilePopup";
 
 export default function Sidebar() {
   const [triggered, setTriggered] = useState(false);
