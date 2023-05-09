b889108fe8e5029564b78fe8605b840f7cdb45f8
carvalho28 <carvalhoo287@gmail.com>
2023-05-09 22:24:43 +0100

    fix: download pem

--- frontend/src/Register.tsx
+++ frontend/src/Register.tsx
@@ -57,16 +57,16 @@ function Register() {
     const keypair = forge.pki.rsa.generateKeyPair({ bits: 2048 });
     const publicKey = forge.pki.publicKeyToPem(keypair.publicKey);
     const privateKey = forge.pki.privateKeyToPem(keypair.privateKey);
+    const element = document.createElement("a");
+    const file = new Blob(["PRIVATE KEY:\n" + privateKey], {
+      type: "text/plain",
+    });
+    element.href = URL.createObjectURL(file);
+    element.download = "privateKey.pem";
+    document.body.appendChild(element);
+    element.click();
+    element.remove();
     return { publicKey, privateKey };
-    // const element = document.createElement("a");
-    // const file = new Blob(["PRIVATE KEY:\n" + privateKey], {
-    //   type: "text/plain",
-    // });
-    // element.href = URL.createObjectURL(file);
-    // element.download = "privateKey.pem";
-    // document.body.appendChild(element);
-    // element.click();
-    // element.remove();
   }
 
   return (
@@ -129,15 +129,22 @@ function Register() {
           </div>
 
           {errorMessage != "" && (
-            <div className="flex items-center justify-center my-3 p-2 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-5 dark:text-red-400 dark:border-red-800" role="alert">
-              <svg 
-                aria-hidden="true" 
-                className="flex-shrink-0 inline w-5 h-5 mr-3" 
-                fill="currentColor" 
-                viewBox="0 0 20 20" 
+            <div
+              className="flex items-center justify-center my-3 p-2 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-5 dark:text-red-400 dark:border-red-800"
+              role="alert"
+            >
+              <svg
+                aria-hidden="true"
+                className="flex-shrink-0 inline w-5 h-5 mr-3"
+                fill="currentColor"
+                viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd"/>
+                <path
+                  fillRule="evenodd"
+                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                  clipRule="evenodd"
+                />
               </svg>
               <span className="sr-only">Info</span>
               <div>
